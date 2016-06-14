18710fe2b5dde208ca9fba9e77b697930c510868
Merhat <merhat1998@gmail.com>
2016-06-14 18:30:41 +0300

    Third homework started.

---
+++ 3. Extension-Methods-Delegates-Lambda-LINQ/ExtensionMethodsDelegatesLambda/ExtensionMethodsDelegatesLambda/Extensions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace ExtensionMethodsDelegatesLambda.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class Extensions
+    {
+        public static StringBuilder Substring (this StringBuilder sb, int index, int length)
+        {
+            StringBuilder res = new StringBuilder();
+            for (int i = index; i < index + length; i++)
+            {
+                res.Append(sb[i]);
+            }
+            return res;
+        }
+
+        public static int Sum<T>(this IEnumerable<T> list)
+        {
+            int res = 0;
+            foreach (var item in list)
+            {
+                res += item;
+            }
+            return 0;
+        }
+    }
+}
---
+++ 3. Extension-Methods-Delegates-Lambda-LINQ/ExtensionMethodsDelegatesLambda/ExtensionMethodsDelegatesLambda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using ExtensionMethodsDelegatesLambda.Extensions;
+
+namespace ExtensionMethodsDelegatesLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("IDubbbz is gay retard");
+            Console.WriteLine(str.Substring(10, 2));
+            string test = "IDubbbz is gay retard";
+            Console.WriteLine(test.Substring(10, 2));
+        }
+    }
+}
